8e894e7f1e0e2787afdd75e3c7a26273c70e0147
Fernando G <fgomez@afianza.net>
2012-02-27 17:03:51 +0100

    simplificando

--- blah.js
+++
@@ -1,3 +0,0 @@
-
-
-<a href="javascript:function randomdni(){var numero=Math.floor(100000000*Math.random());var letras='TRWAGMYFPDXBNJZSQVHLCKE';var indice=numero % 23;try{document.activeElement.value=numero.toString()+letras.charAt(indice);}catch(e){alert('No hay elemento de form seleccionado. El DNI generado es '+numero.toString()+letras.charAt(indice));}}randomdni();void(0)">Generar DNI</a>
